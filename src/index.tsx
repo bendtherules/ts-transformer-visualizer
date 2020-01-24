0ba1a0197307fffc51ce1f4cf56014d1a3938442
bendtherules <abhasbhattacharya2@gmail.com>
2020-01-24 18:15:10 +0530

    index.tsx - Consume store in component

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,26 @@
  */
 
 import * as React from "react";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+import { initSourceFile, updateSourceFile } from "./store/sourceFiles/actions";
 
 import styles from "./styles.css";
 
 export type Props = { text: string };
 
-export default class ExampleComponent extends React.Component<Props> {
+const store = configureStore();
+
+export class ExampleComponent extends React.Component<Props> {
   render() {
     const { text } = this.props;
 
-    return <div className={styles.test}>Example Component: {text}</div>;
+    return (
+      <Provider store={store}>
+        <div className={styles.test}>Example Component: {text}</div>
+      </Provider>
+    );
   }
 }
+
+export { initSourceFile, updateSourceFile };
